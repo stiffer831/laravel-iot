beb233f3a66866268f13212ea1251ccf8f3ca3d7
stiffer <2181867045@qq.com>
2022-04-06 10:19:20 +0800

    添加Helper辅助文件

---
+++ app/Util/Helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Helpers.php
+ *
+ * @copyright 2022 wkld.com - All Rights Reserved
+ * @link https://www.wkld.com
+ * @author stiffer.chen <2181867045@qq.com>
+ * @created 2022-04-06 10:14:29
+ * @modified 2022-04-06 10:14:29
+ */
+
+if (!function_exists('register')) {
+    /**
+     * @param $key
+     * @param $value
+     * @param $force
+     *
+     * @return void
+     */
+    function register($key, $value, $force = false)
+    {
+        \App\Library\Registry::set($key, $value, $force);
+    }
+}
+
+if (!function_exists('registry')) {
+    /**
+     * @param $key
+     * @param $default
+     *
+     * @return mixed|null
+     */
+    function registry($key, $default = null)
+    {
+        return \App\Library\Registry::get($key, $default);
+    }
+}
