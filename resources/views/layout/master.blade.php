da82b0962ed3cced73d84fb5ab98d10548b245ea
stiffer <2181867045@qq.com>
2022-04-07 15:53:52 +0800

    add js and css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   @section('header_css')
     <link rel="stylesheet" href="{{ asset('static/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('static/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('static/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('static/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('static/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('static/plugins/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('static/plugins/summernote/summernote-bs4.min.css') }}">
   @show
   @section('header_js')
 
@@ -19,13 +24,7 @@
   {{-- left start --}}
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-      <a href="index3.html" class="nav-link">Home</a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-      <a href="#" class="nav-link">Contact</a>
+      <a class="nav-link" data-widget="pushmenu" href="javascript:;" role="button"><i class="fas fa-bars"></i></a>
     </li>
   </ul>
   {{-- left end --}}
@@ -48,8 +47,8 @@
 {{-- left sidebar start --}}
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <div class="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <img src="{{ asset('static/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
   </div>
   <div class="sidebar">
     <nav class="mt-2">
@@ -95,7 +94,7 @@
 {{-- content end --}}
 {{-- footer start --}}
 <footer class="main-footer text-center">
-  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+  <strong>Copyright &copy; 2020-{{ date('Y') }} <a href="javascript:;">瑞怡STACK</a>.</strong>
   All rights reserved.
 </footer>
 {{-- footer end --}}
@@ -104,6 +103,22 @@
 
 @show
 @section('footer_js')
-
+  <script src="{{ asset('static/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+  <script type="text/javascript">
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <script src="{{ asset('static/bootstrap/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/sparklines/sparkline.js') }}"></script>
+  <script src="{{ asset('static/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/moment/moment.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/daterangepicker/daterangepicker.js') }}"></script>
+  <script src="{{ asset('static/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/summernote/summernote-bs4.min.js') }}"></script>
+  <script src="{{ asset('static/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+  <script src="{{ asset('static/js/adminlte.js') }}"></script>
+  <script src="{{ asset('static/js/demo.js') }}"></script>
+  <script src="{{ asset('static/js/dashboard.js') }}"></script>
 @show
 </html>
