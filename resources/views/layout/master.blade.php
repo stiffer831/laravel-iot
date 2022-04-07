29cbf27de3171463c324bd3f0c481d7eea747018
stiffer <2181867045@qq.com>
2022-04-07 13:15:46 +0800

    prepare npm

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,20 +1,104 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @section('header_css')
-        <link rel="stylesheet" href="{{ asset('static/fontawesome/css/all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('static/css/adminlte.min.css') }}">
-    @show
-    @section('header_js')
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @section('header_css')
+    <link rel="stylesheet" href="{{ asset('static/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('static/css/adminlte.min.css') }}">
+  @show
+  @section('header_js')
 
-    @show
-    <title>@yield('heading_title') - {{ config('app.name') }}</title>
+  @show
+  <title>@yield('heading_title') - {{ config('app.name') }}</title>
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
-
+{{-- top nav start --}}
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  {{-- left start --}}
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="index3.html" class="nav-link">Home</a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="#" class="nav-link">Contact</a>
+    </li>
+  </ul>
+  {{-- left end --}}
+  {{-- right start --}}
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+        <i class="fas fa-expand-arrows-alt"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+        <i class="fas fa-th-large"></i>
+      </a>
+    </li>
+  </ul>
+  {{-- right end --}}
+</nav>
+{{-- top nav start end --}}
+{{-- left sidebar start --}}
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <div class="brand-link">
+    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
+  </div>
+  <div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item menu-open">
+          <a href="#" class="nav-link active">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+              Dashboard
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./index.html" class="nav-link active">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v1</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./index2.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v2</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./index3.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v3</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</aside>
+{{-- left sidebar end --}}
+{{-- content start --}}
+<div class="content-wrapper">
+  content
+</div>
+{{-- content end --}}
+{{-- footer start --}}
+<footer class="main-footer text-center">
+  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+  All rights reserved.
+</footer>
+{{-- footer end --}}
 </body>
 @section('footer_css')
 
--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -1,58 +1,56 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{{ asset('static/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('static/bootstrap/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('static/js/adminlte.min.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('static/fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('static/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('static/css/adminlte.min.css') }}">
-    <title>{{ __('login.title') }} - {{ config('app.name') }}</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="{{ asset('static/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('static/bootstrap/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('static/js/adminlte.min.js') }}"></script>
+  <link rel="stylesheet" href="{{ asset('static/fontawesome/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('static/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('static/css/adminlte.min.css') }}">
+  <title>{{ __('login.title') }} - {{ config('app.name') }}</title>
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
-    <div class="login-logo">
-        {{ config('app.name') }}
-    </div>
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">{{__('login.welcome')}}{{ config('app.name') }}</p>
-            @if(session('warning'))
-                <div class="alert alert-danger alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    {{ session('warning') }}
-                </div>
-            @endif
-            <form action="{{ route('login.submit') }}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div class="input-group mb-3">
-                    <input type="text" name="username" value="{{ old('username') }}" class="form-control" placeholder="{{ __('login.username') }}">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control"
-                           placeholder="{{ __('login.password') }}">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary btn-block">{{ __('login.submit') }}</button>
-                    </div>
-                </div>
-            </form>
+  <div class="card">
+    <div class="card-body login-card-body">
+      <img src="{{ asset('static/img/logo_text.png') }}" alt="{{ config('app.name') }}">
+      @if(session('warning'))
+        <div class="alert alert-danger alert-dismissible">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          {{ session('warning') }}
         </div>
+      @endif
+      <form action="{{ route('login.submit') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="text" name="username" value="{{ old('username') }}" class="form-control"
+                 placeholder="{{ __('login.username') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control"
+                 placeholder="{{ __('login.password') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">{{ __('login.submit') }}</button>
+          </div>
+        </div>
+      </form>
     </div>
+  </div>
 </div>
 </body>
 </html>
